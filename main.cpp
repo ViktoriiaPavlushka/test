22f9eb13ad7b3336881794e0747ee1fdd29f80ff
ViktoriiaPavlushka <viktoriia.pavliukh.shi.2023@lpnu.ua>
2023-10-29 20:58:16 +0200

    Changed main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@ int main() {
     // Declare a string variable named 'text' and initialize it with the text "Hello World!"
     std::string text = "Hello World!";
     // Output the contents of the 'text' variable to the console using 'std::cout'
-    std::cout << text; 
+    std::cout << text;
+    
     // Return 0 to indicate a successful program execution
     return 0;
 }
